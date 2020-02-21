c7b8bb60a3c0a237a262122dd8476dbc1f6dc734
JDMUSC <user>
2020-02-21 05:16:23 -0500

    simpler

--- READTV/eventsDisplayServer.R
+++ READTV/eventsDisplayServer.R
@@ -1,40 +1,4 @@
 
-
-eventsDisplayUI <- function(id) {
-  ns = NS(id)
-  fluidPage(
-    shinyjs::useShinyjs(),
-    shinyjs::inlineCSS(list(.invalid_query = 'background-color: #f006')),
-    tabsetPanel(
-      tabPanel(
-        "Data Upload",
-        div(
-          actionButton(ns("minimizeHeader"), "Minimize"),
-          uiOutput(ns("headerInformation")),
-          div(id = ns("loadDataHeader"),
-              fluidRow(
-                eventsLoaderUI(ns("loadData"))),
-              fluidRow(
-                metaQueryLoaderUI(ns("loadMetaData")),
-                metaQueryUI(ns("metaqueryui"))
-                )
-          )
-        )
-      ),
-      tabPanel(
-        "Display",
-        div(
-          uiOutput(ns("dataFilter")),
-          fluidRow(
-            column(uiOutput(ns("eventPlotContainer")), width = 10),
-            column(uiOutput(ns("sidePanel")), width = 2)
-          )
-        )
-      )
-    )
-  )
-}
-
 eventsDisplayServer = function(input, output, session){
   ns = session$ns
   
