8e67a275456d2ae90e76982d71790035a9ec4094
JDMUSC <user>
2020-09-08 14:19:25 -0400

    source unexpected

--- READTV/launch.R
+++ READTV/launch.R
@@ -1,5 +1,3 @@
-launchReadtv = function(eventsPath = NULL) {
-  source('global.R') #keep in function to prevent recursion
-  
-  shinyApp(ui = genUi(), server = genServerFn(eventsPath = eventsPath))
-}
+launchReadtv = function(eventsPath = NULL)
+  shinyApp(ui = genUi(), 
+           server = genServerFn(eventsPath = eventsPath))
